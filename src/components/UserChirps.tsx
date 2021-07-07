92d0d0c90dbc227b0cad70244911f09e98086e34
marcski55 <marc.skwarczynski@revature.net>
2021-07-07 12:17:49 -0700

    Finally got user page working.

--- src/components/UserChirps.tsx
+++ src/components/UserChirps.tsx
@@ -4,8 +4,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { GetUsersChirps } from '../actions/ChirpsActions';
 import { RootStore } from '../store/store';
 
-  
-
 export const UserChirps: React.FC = () => {
   const dispatch = useDispatch();
   const chirpsState = useSelector((state: RootStore) => state.chirps);
@@ -14,9 +12,8 @@ export const UserChirps: React.FC = () => {
     dispatch(GetUsersChirps());
   }
 
+  // eslint-disable-next-line
   React.useEffect(() => loadData(),[]);
-
-  console.log(chirpsState.chirps);
 
   return (<>
     {chirpsState.chirps && chirpsState.chirps.map((chirp, index) => {
@@ -26,11 +23,11 @@ export const UserChirps: React.FC = () => {
           <img alt="profile pic" src={"https://64.media.tumblr.com/1c0a550b6a6b075c35bb0f62e6b14047/580b88b831872a09-e8/s250x400/04b15506e7f9c7e11a3aa86f4373c0acb4ddb9c9.png" || chirp.img.S} width="64px"></img>
           </Col>
           <Col xs="8">
-            <span className="chirp-user">@{chirp.username.S}</span>
+            <span className="chirp-user">@{chirp.username}</span>
             <br></br>
-            <span className="chirp-body">{chirp.body.S}</span>
+            <span className="chirp-body">{chirp.body}</span>
             <br></br>
-            <span className="chirp-time">{(new Date(Number(chirp.timestamp.S))).toLocaleString('en-US', {timeZone: 'EST'})}</span>
+            <span className="chirp-time">{(new Date(Number(chirp.timestamp))).toLocaleString('en-US', {timeZone: 'EST'})}</span>
           </Col>
           {/* <Col xs="2" className="my-auto">
             <span className="likes-label">{chirp.likes.L.length} <button className="like-button">♥</button></span>
